e32378c0e7a65d18c4f888d70b871d16a4dcea30
Ishaan Jain <ishaan160799@gmail.com>
2020-09-19 08:40:46 +0530

    mobileapp: login ui done

--- mobile-app/app/src/main/java/com/systemtron/neki/activities/SplashActivity.kt
+++ mobile-app/app/src/main/java/com/systemtron/neki/activities/SplashActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.systemtron.neki.R
+import com.systemtron.neki.utils.Tags
 
 class SplashActivity : AppCompatActivity() {
 
@@ -13,9 +15,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Log.d(Tags.ishaanTag, "Splash -> Login")
         Handler().postDelayed({
             val splashIntent = Intent(this, LoginActivity::class.java)
             startActivity(splashIntent)
-        }, 3000)
+        }, splashTimeOut.toLong())
     }
 }
