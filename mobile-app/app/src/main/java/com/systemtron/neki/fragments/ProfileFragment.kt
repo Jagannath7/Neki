32e1a226de1d338b9e984ca478b53c1e1b56cfb8
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-09-19 17:34:58 +0530

    mobile app: updated profile ui

--- mobile-app/app/src/main/java/com/systemtron/neki/fragments/ProfileFragment.kt
+++ mobile-app/app/src/main/java/com/systemtron/neki/fragments/ProfileFragment.kt
@@ -1,18 +1,39 @@
 package com.systemtron.neki.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.getField
+import com.google.firebase.ktx.Firebase
 import com.systemtron.neki.R
+import com.systemtron.neki.utils.Constants
+import com.systemtron.neki.utils.Tags
+import kotlinx.android.synthetic.main.fragment_profile.*
+import java.util.*
 
 class ProfileFragment : Fragment() {
+
+    private val currentUser by lazy {
+        Firebase.auth.currentUser
+    }
+
+    private val database by lazy {
+        Firebase.firestore
+    }
+    private var name: String = ""
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
