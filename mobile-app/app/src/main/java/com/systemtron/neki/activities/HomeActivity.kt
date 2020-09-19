1054398f97b04b9a7c2059f3978174dbefa0717a
Ishaan Jain <ishaan160799@gmail.com>
2020-09-19 12:08:41 +0530

    mobile app: fragments added

--- mobile-app/app/src/main/java/com/systemtron/neki/activities/HomeActivity.kt
+++ mobile-app/app/src/main/java/com/systemtron/neki/activities/HomeActivity.kt
@@ -3,10 +3,12 @@ package com.systemtron.neki.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.systemtron.neki.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
     }
 }
