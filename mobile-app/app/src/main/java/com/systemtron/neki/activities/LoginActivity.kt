a97c65d1594ba4d2d090f4745f1ae2d8bd019733
Ishaan Jain <ishaan160799@gmail.com>
2020-09-19 09:01:31 +0530

    mobileapp: login kotlin code done

--- mobile-app/app/src/main/java/com/systemtron/neki/activities/LoginActivity.kt
+++ mobile-app/app/src/main/java/com/systemtron/neki/activities/LoginActivity.kt
@@ -2,11 +2,108 @@ package com.systemtron.neki.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.systemtron.neki.R
+import com.systemtron.neki.utils.Tags
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val auth by lazy {
+        Firebase.auth
+    }
+
+    private val currentUser by lazy {
+        auth.currentUser
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            val email = etEmailId.editText?.text.toString()
+            val password = etPassword.editText?.text.toString()
+
+            if (email.isEmpty() or password.isEmpty()) {
+                Toast.makeText(this, "Please Enter Email Id or Password", Toast.LENGTH_SHORT).show()
+            } else {
+                checkEmail(email, password)
+            }
+        }
+    }
+
+    private fun checkEmail(email: String, password: String) {
+        auth.fetchSignInMethodsForEmail(email)
+            .addOnCompleteListener {
+                if (it.result?.signInMethods?.isEmpty()!!) {
+                    Log.d(Tags.ishaanTag, "Sign Up at fetchSignInMethods")
+                    signUp(email, password)
+                } else {
+                    Log.d(Tags.ishaanTag, "Sign In at fetchSignInMethods")
+                    signIn(email, password)
+                }
+            }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    try {
+                        throw it.exception!!
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthInvalidUserException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthUserCollisionException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthWeakPasswordException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }.addOnSuccessListener {
+                Toast.makeText(this, "Sign In Successful", Toast.LENGTH_SHORT).show()
+                Log.d(Tags.ishaanTag, "Sign in Successful")
+            }.addOnFailureListener {
+                Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show()
+                Log.d(Tags.ishaanTag, "Sign in Failed")
+            }
+    }
+
+    private fun signUp(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    try {
+                        throw it.exception!!
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthInvalidUserException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthUserCollisionException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthWeakPasswordException) {
+                        Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }.addOnSuccessListener {
+                Toast.makeText(this, "Sign Up Successful", Toast.LENGTH_SHORT).show()
+                Log.d(Tags.ishaanTag, "Sign up Successful")
+            }.addOnFailureListener {
+                Toast.makeText(this, "Sign Up Failed", Toast.LENGTH_SHORT).show()
+                Log.d(Tags.ishaanTag, "Sign up Failed")
+            }
+    }
+
+    override fun onBackPressed() {
+        finishAffinity()
     }
 }
