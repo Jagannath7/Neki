c5718ead9e9ede6613a2855cb0036dbab35b4146
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-09-20 10:04:03 +0530

    web: 404 andded and some minor bug fix

--- web-app/client/src/components/Section2.js
+++ web-app/client/src/components/Section2.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
