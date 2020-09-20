06c6fb99e07799bb82dd3c262eb620102ef96c7c
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-09-20 09:38:00 +0530

    web: Login Minor Fix

--- web-app/client/src/components/Login.js
+++ web-app/client/src/components/Login.js
@@ -3,19 +3,14 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import firebase from '../config/fire';
+import firebase from "../config/fire";
 import { auth } from "firebase";
-
-
 
 function Copyright() {
   return (
@@ -72,10 +67,17 @@ export default function SignIn() {
   const handleForm = (e) => {
     e.preventDefault();
     console.log(formData);
-    firebase.auth().createUserWithEmailAndPassword(formData.email,formData.password)
-    .then(res=>{if(res.user){
-      auth.setLoggedIn(true);
-    } }).catch(err=>{console.log(err)})
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(formData.email, formData.password)
+      .then((res) => {
+        if (res.user) {
+          auth.setLoggedIn(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <Container component="main" maxWidth="xs">
@@ -85,7 +87,7 @@ export default function SignIn() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Sign In
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
